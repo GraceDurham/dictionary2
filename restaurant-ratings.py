1c0bd6879185ca99050075aeb9352a5515fc94e7
Hackbright Student <no-reply@hackbrightacademy.com>
2017-01-09 17:09:16 -0800

    Dictionary updated with unpacking

--- restaurant-ratings.py
+++ restaurant-ratings.py
@@ -3,12 +3,13 @@ restaurant_file = open("scores.txt")
 restaurant_ratings = {}
 
 for line in restaurant_file:
-    line = line.split(":")
-    restaurant_name = line[0]
-    rating = line[1]
+    line = line.rstrip().split(":")
+    restaurant_name, rating = line 
+    # restaurant_name = line[0]
+    # rating = line[1]
     restaurant_ratings[restaurant_name] = rating
     restaurant_rate = sorted(restaurant_ratings.items())
     # print restaurant_rate
 
 for restaurant_name, rating in restaurant_rate:
-    print "%s is rated at %s" % (restaurant_name, rating)
+    print "%s is rated at %s." % (restaurant_name, rating)
